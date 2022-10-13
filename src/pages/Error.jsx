9d3bb827503f2be9726d82bc07b53459a73b5182
LauNM <laurie.beduneau@gmail.com>
2022-10-13 11:47:11 +0200

    fixed word spelling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Error() {
   return (
     <div className={"error"}>
-      <h1>Oups, une erreure s'est produite</h1>
+      <h1>Oups, une erreur s'est produite</h1>
       <p>Veuillez rééssayer dans quelques instants</p>
       <Link to={'/'} >
            <button className={"link-button"}>Revenir à la page d'accueil</button>
